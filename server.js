d86c204f7f064c328512f4ca6e1f3b6f3085d857
unknown <rehabdaas2004@gmail.com>
2025-03-16 13:29:30 +0400

    Initial commit with basic setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample Route
+app.get('/', (req, res) => {
+    res.send('Insurance API is running...');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
